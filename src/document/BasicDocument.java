31dc69a9291f648c526101fd3fb3c481a8267faa
DESKTOP-JLCLQGL\shivang17 <shivang170695@gmail.com>
2017-07-14 00:20:25 +0530

    Initial Commit

---
+++ src/document/BasicDocument.java
@@ -0,0 +1,86 @@
+package document;
+
+import java.util.List;
+
+
+
+public class BasicDocument extends Document 
+{
+	
+	
+	public BasicDocument(String text)
+	{
+		super(text);
+	}
+	
+	
+	public int getNumWords()
+	{
+		List<String> tokens= getTokens( " [a-zA-Z]+");
+		return tokens.size();
+		
+	}
+	
+	
+	public int getNumSentences()
+	{
+		List<String> tokens= getTokens("[^!?.]+");
+		return tokens.size();
+	}
+	protected int countSyllables(String word)
+	{
+	    //System.out.print("Counting syllables in " + word + "...");
+		int numSyllables = 0;
+		boolean newSyllable = true;
+		String vowels = "aeiouy";
+		char[] cArray = word.toCharArray();
+		for (int i = 0; i < cArray.length; i++)
+		{
+		    if (i == cArray.length-1 && Character.toLowerCase(cArray[i]) == 'e' 
+		    		&& newSyllable && numSyllables > 0) {
+                numSyllables--;
+            }
+		    if (newSyllable && vowels.indexOf(Character.toLowerCase(cArray[i])) >= 0) {
+				newSyllable = false;
+				numSyllables++;
+			}
+			else if (vowels.indexOf(Character.toLowerCase(cArray[i])) < 0) {
+				newSyllable = true;
+			}
+		}
+		//System.out.println( "found " + numSyllables);
+		return numSyllables;
+	}
+	
+	
+	
+	
+	public static void main(String[] args)
+	{
+		
+		testCase(new BasicDocument("This is a test.  How many???  "
+		        + "Senteeeeeeeeeences are here... there should be 5!  Right?"),
+				16, 13, 5);
+		testCase(new BasicDocument(""), 0, 0, 0);
+		testCase(new BasicDocument("sentence, with, lots, of, commas.!  "
+		        + "(And some poaren)).  The output is: 7.5."), 15, 11, 4);
+		testCase(new BasicDocument("many???  Senteeeeeeeeeences are"), 6, 3, 2);
+		testCase(new BasicDocument("Here is a series of test sentences. Your program should "
+				+ "find 3 sentences, 33 words, and 49 syllables. Not every word will have "
+				+ "the correct amount of syllables (example, for example), "
+				+ "but most of them will."), 49, 33, 3);
+		testCase(new BasicDocument("Segue"), 2, 1, 1);
+		testCase(new BasicDocument("Sentence"), 2, 1, 1);
+		testCase(new BasicDocument("Sentences?!"), 3, 1, 1);
+		testCase(new BasicDocument("Lorem ipsum dolor sit amet, qui ex choro quodsi moderatius, nam dolores explicari forensibus ad."),
+		         32, 15, 1);
+	}
+
+
+	@Override
+	public int getNumSyllables() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+}
